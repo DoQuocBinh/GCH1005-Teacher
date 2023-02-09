ddbcd57ffe2a83dace6340f552b46882356f9f3f
DoQuocBinh <binhdq@fpt.edu.vn>
2023-02-09 11:29:32 +0700

    file

--- expressDemo/main.js
+++ expressDemo/main.js
@@ -12,6 +12,29 @@ app.get('/',(req,res)=>{
 })
 
 app.use(express.urlencoded({extended:true}))//allow to read data from HTML elements
+
+app.get('/event',(req,res)=>{
+    res.render("event")
+})
+
+app.post('/event',(req,res)=>{
+    let name = req.body.txtName
+    let event = req.body.event
+    fs.appendFileSync('data2.txt','\n'+name + ';' +event)
+    //read File to view on the home page
+    let fileContent = fs.readFileSync("data2.txt",'utf-8')
+    let lines = fileContent.trim().split("\n")
+    let events = []
+    for(i=0;i<lines.length;i++){
+        let student = {
+            name : lines[i].split(';')[0],
+            event : lines[i].split(';')[1]
+        }
+        events.push(student)
+    }
+    res.render('home',{name:name,events:events})
+})
+
 app.post('/register',(req,res)=>{
     let name = req.body.txtName
     let email = req.body.txtEmail
