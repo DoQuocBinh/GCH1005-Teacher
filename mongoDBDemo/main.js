2d3313ee18d6f3668f79ad00bcca66d3fca2ae5a
DoQuocBinh <binhdq@fpt.edu.vn>
2023-02-23 08:05:56 +0700

    ok

---
+++ mongoDBDemo/main.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+
+app.set('view engine','hbs')
+app.use(express.urlencoded({extended:true}))
+
+var url = 'mongodb://127.0.0.1:27017';
+var MongoClient = require('mongodb').MongoClient;
+
+app.get('/',async (req,res)=>{
+    let client = await MongoClient.connect(url)
+    let dbo = client.db("GCH1005")
+    let products = await dbo.collection("products").find().toArray()
+    res.render('main',{'products':products})
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT,()=>{
+    console.log("Server is up!")
+})
