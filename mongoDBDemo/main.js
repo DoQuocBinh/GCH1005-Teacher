be615b10619f3259f3b6c8b79988a4cf4d446f99
DoQuocBinh <binhdq@fpt.edu.vn>
2023-02-28 08:03:16 +0700

    ok

--- mongoDBDemo/main.js
+++ mongoDBDemo/main.js
@@ -7,10 +7,41 @@ app.use(express.urlencoded({extended:true}))
 var url = 'mongodb://127.0.0.1:27017';
 var MongoClient = require('mongodb').MongoClient;
 
+app.post('/edit',async (req,res)=>{
+    const name = req.body.txtName
+    const price = req.body.txtPrice
+    const picURL = req.body.picURL
+    const id = req.body.id
+
+    let client = await MongoClient.connect(url)
+    let dbo = client.db("GCH1005")
+    var ObjectId = require('mongodb').ObjectId
+    const condition = {"_id" : new ObjectId(id)};
+    const newValues = {$set : {name:name,price:price,picURL:picURL}}
+    await dbo.collection("products").updateOne(condition,newValues)
+    res.redirect('/')
+})
+
+app.get('/edit/:id', async(req,res)=>{
+    const id = req.params.id
+    let client = await MongoClient.connect(url)
+    let dbo = client.db("GCH1005")
+    var ObjectId = require('mongodb').ObjectId
+    let condition = {"_id" : new ObjectId(id)};
+    const prod = await dbo.collection("products").findOne(condition)
+    res.render('edit',{prod:prod})
+})
+
 app.post('/add',async (req,res)=>{
     const name = req.body.txtName
     const price = req.body.txtPrice
     const picURL = req.body.picURL
+    //kiem tra input
+    if(name.length <=5){
+        res.render('add',{name_err: 'Min length is 5 characters'})
+        return
+    }
+    //
     const newProduct = {
         'name': name,
         'price':price,
